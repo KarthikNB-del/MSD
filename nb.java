53187520a0ab059da3513d3140d6eab570d8f460
Karthik NB <karthiknb984@gmail.com>
2025-03-24 11:34:55 +0530

    Update nb.java

--- nb.java
+++ nb.java
@@ -6,22 +6,8 @@ public class nb{
     // main function
     public static void main(String[] args)
     {
-        // Declare the variables
-        int num;
-
         // Input the integer
         System.out.println("Enter the integer: ");
 
-        // Create Scanner object
-        Scanner s = new Scanner(System.in);
-
-        // Read the next integer from the screen
-        num = s.nextInt();
-
-        // Display the integer
-        System.out.println("Entered integer is: " + num);
-
-        // Close the Scanner object
-        s.close(); // Important to prevent resource leaks
     }
 }
